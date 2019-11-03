6b53bababba7159757f07ade992ed87554672994
gcheca <gcheca666@gmail.com>
2019-11-03 19:15:59 +0100

    Create PEC5.c

---
+++ PEC5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define publicationDay[10] "11/05/2019"
+
+int main(int argc, char **argv)
+{
+  printf("This PEC code will be published on %s", publicationDay);
+  
+  return 0;
+}
